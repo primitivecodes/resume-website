076951d6fb48370a5fdd1db8943e277e54f49f61
primitivecodes <p.ufitinema@alustudent.com>
2025-06-19 06:46:04 -0700

    updated css,javascript and html of the resume website

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["a Full-stack developer","a Web designer","an AI enthusiast","a Data analyst","a Front-end development trainer"],
+    typeSpeed:110,
+    BackSpeed:60,
+    loop:true
+})
